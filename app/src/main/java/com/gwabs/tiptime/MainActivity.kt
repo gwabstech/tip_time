3391210c4ccce85051ff03678972f197ce3d7b43
gwabstech <72021618+gwabstech@users.noreply.github.com>
2022-03-31 12:13:30 +0100

    Done

--- app/src/main/java/com/gwabs/tiptime/MainActivity.kt
+++ app/src/main/java/com/gwabs/tiptime/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.gwabs.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gwabs.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    // declare the binding object
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // initialize the binding variable i created
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+           calculateTips()
+        }
+
+
+    }
+
+    private fun calculateTips(){
+        // here i get the cost of service and save it to a variable
+        val costOfService = binding.costOfService.text.toString()
+        val cost = costOfService.toDoubleOrNull()
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent-> 0.18
+            else->0.15
+        }
+
+        if (cost == null){
+            binding.costOfService.error = "Please enter cost of service"
+            return
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        if (roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
